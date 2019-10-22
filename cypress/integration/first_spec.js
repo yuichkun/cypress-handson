c5e377e36670d5d10ae9f2b35b41eb4a5a677b14
Yuichi Yogo <yuichkun@gmail.com>
2019-10-22 20:04:06 +0900

    chore: add cypress folder

---
+++ cypress/integration/first_spec.js
@@ -0,0 +1,7 @@
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    cy.visit('https://example.cypress.io')
+    cy.contains('children').click();
+    cy.url();
+  })
+})
